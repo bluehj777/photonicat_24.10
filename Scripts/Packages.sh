a26729d9850887f2ea9afbc5a17c1cd6ed095d09
bluehj777 <huajia@jiahua.org.cn>
2025-08-15 15:32:38 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -74,6 +74,7 @@ UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-ap
 UPDATE_PACKAGE "luci-app-xupnpd" "jarod360/luci-app-xupnpd" "main"
 UPDATE_PACKAGE "luci-app-onliner" "bluehj777/openwrt_pkgs" "main" "pkg"
 UPDATE_PACKAGE "luci-app-socat" "bluehj777/openwrt_pkgs" "main" "pkg"
+UPDATE_PACKAGE "luci-app-gpoint" "Kodo-kakaku/luci-app-gpoint" "main"
 #UPDATE_PACKAGE "pcat-manager" "ntbowen/pcat-manager" "main"
 #UPDATE_PACKAGE "luci-mod-photonicatstatus" "ntbowen/luci-mod-photonicatstatus" "main"
 
